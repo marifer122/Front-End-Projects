03bd1f2b71314a21fda30b058b82187246190b7e
marifer122 <mariferguson244@gmail.com>
2025-02-17 16:51:47 -0500

    Update palindrome.js

--- Projects/palindrome/palindrome.js
+++ Projects/palindrome/palindrome.js
@@ -1,20 +1,27 @@
 function palindrome_check(){
   var str1 = document.getElementById("input").value;
-  var str=str1.replace(/\s/g, "");
-  if(isPalindrome(str)===true){
-  document.getElementById("prog").innerHTML = `<b>"${str}"</b> is a palindrome!.`;
+  //conly allow letters
+  var str=str1.replace(/[a-zA-Z0-9]/g,");
+  
+  // scheck for empty string
+  if (str === ""){
+      return;
+  }
+    
+  if(isPalindrome(str)){
+  document.getElementById("prog").innerHTML = `<b>"${str1}"</b> is a palindrome!.`;
   document.getElementById("input").value="";
   
 }
   else
-  document.getElementById("prog").innerHTML = `<b>"${str}"</b> is not a palindrome!.`;
+  document.getElementById("prog").innerHTML = `<b>"${str1}"</b> is not a palindrome!.`;
 }
 function isPalindrome(str1) {
-  str=str1.toLowerCase();
+  var str=str1.toLowerCase();
   for (let i = 0; i < Math.floor(str.length / 2); i++)
   {
-    var char1=str[i],char2=str[str.length - 1 - i];
-    if (char1 !== char2)
+      //make a comparison to find palindrome
+    if ( str[i] !== str[str.length - 1 - i])
     {
       return false;
     }
